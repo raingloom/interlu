9431e60b5fd6124875b3f77f65d17af33b607563
Rain Gloom <raingloom@users.noreply.github.com>
2017-04-17 22:51:18 +0200

    probably fixed #3

--- init.lua
+++ init.lua
@@ -1,16 +1,30 @@
 local M = {}
 
-local pcall, tostring, type, require, loadstring, io_stderr, table_insert, table_concat, table_pack, table_unpack, print =
-       pcall, tostring, type, require, loadstring, io.stderr, table.insert, table.concat, table.pack, table.unpack, print
+local pcall, tostring, type, require, loadstring, io_stderr, io_write, io_read, table_insert, table_concat, table_pack, table_unpack, print =
+       pcall, tostring, type, require, loadstring, io.stderr, io.write, io.read, table.insert, table.concat, table.pack, table.unpack, print
+
+local readline
+do
+	local ok, libreadline = pcall( require, 'readline' )
+	if ok then
+		readline = libreadline.readline
+	else
+		function readline( prompt )
+			io_write( promp )
+			return io_read()
+		end
+	end
+end
 
 local function isIncomplete( err )
+	--[[U G L Y    H A C K]]--
 	return not not err:match'expected.-near %<eof%>%s*$'
 end
 
 local function writeerr( err )
 	local ok, str = pcall( tostring, err )
 	if not ok then
-		str = 'error is (' .. type( err ) .. ')'
+		str = 'error object is a (' .. type( err ) .. ')'
 	end
 	io_stderr:write( str )
 	io_stderr:write'\n'
@@ -25,14 +39,13 @@ local function runfun( fun )
 	end
 end
 
-local readline = require 'readline'
 local level1 = true
 local canreturn = false
 local buf = {}
 
 function M.interact()
 	while true do
-		local line = readline.readline( level1 and (_PROMPT or '>') or (_PROMP2 or '>>'))
+		local line = readline( level1 and (_PROMPT or '> ') or (_PROMP2 or '>> '))
 		if line then
 			if level1 then
 				local fun, err
@@ -73,6 +86,7 @@ function M.interact()
 					if isIncomplete( err ) then
 						--keep reading
 					else
+						level1 = true
 						writeerr( err )
 					end
 				end
@@ -82,6 +96,5 @@ function M.interact()
 		end
 	end
 end
-
 
 return M
